40409f5fda3dc1c9265f516b7f95990d60e740fe
Khoa <khoa.h.nguyen@tuni.fi>
2023-01-18 12:33:37 +0200

    Refine the UI, adding effects and endgame conditions

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import {
   VStack,
   Button,
   useToast,
-  Heading,
 } from "@chakra-ui/react";
 import LetterTile from "./components/LetterTile";
 import KeyboardTile from "./components/KeyboardTile";
+import Confetti from "react-confetti";
+import { useWindowSize } from "react-use";
 
 import { useState } from "react";
 import { nanoid } from "nanoid";
@@ -64,13 +65,17 @@ function App() {
   const [currentRow, setCurrentRow] = useState(0);
   // Letter counting starts from 0 to 4
   const [currentLetter, setCurrentLetter] = useState(0);
+  // Game is over or not
+  const [isOver, setIsOver] = useState(false);
+  // Game is won or not
+  const [isWin, setIsWin] = useState(false);
   // ChakraUI's toast to announce game state
   const toast = useToast();
 
   // Functions of the game: pressing the keyboard buttons
   function pressLetter(letter) {
     let currentBoard = [...board];
-    if (currentLetter < 5) {
+    if (currentLetter < 5 && !isOver) {
       currentBoard[currentRow][currentLetter] = {
         letter: letter,
         state: "init",
@@ -79,90 +84,106 @@ function App() {
       setBoard(currentBoard);
     }
   }
-
   function pressEnter() {
-    if (currentLetter === 5) {
-      const guessedWord = board[currentRow]
-        .map((l) => l.letter)
-        .join("")
-        .toUpperCase();
-      const isInValidGuesses = [...validGuesses, ...wordList].findIndex(
-        (o) => o.word.toUpperCase() === guessedWord
-      );
-      if (isInValidGuesses >= 0) {
-        // Word checking
-        board[currentRow].forEach((c, iOfGuess) => {
-          const guessedChar = c.letter;
-          const iOfWordToGuess = wordToGuess.indexOf(guessedChar);
-          const currentBoard = [...board];
-          const currentKeyboard = [...keyboard];
+    if (!isOver) {
+      if (currentLetter === 5) {
+        const guessedWord = board[currentRow]
+          .map((l) => l.letter)
+          .join("")
+          .toUpperCase();
+        const isInValidGuesses = [...validGuesses, ...wordList].findIndex(
+          (o) => o.word.toUpperCase() === guessedWord
+        );
+        if (isInValidGuesses >= 0) {
+          // Word checking
+          board[currentRow].forEach((c, iOfGuess) => {
+            const guessedChar = c.letter;
+            const iOfWordToGuess = wordToGuess.indexOf(guessedChar);
+            const currentBoard = [...board];
+            const currentKeyboard = [...keyboard];
 
-          const updateLetterBoards = (letterToUpdate) => {
-            currentBoard[currentRow][iOfGuess] = letterToUpdate;
-            for (let j = 0; j < 3; j++) {
-              const indexToUpdate = currentKeyboard[j].findIndex(
-                (ch) => ch.letter === guessedChar
-              );
-              currentKeyboard[j][indexToUpdate] = letterToUpdate;
-            }
-          };
-
-          if (iOfWordToGuess < 0) {
-            const letterToUpdate = {
-              state: "wrongLetter",
-              letter: c.letter,
+            const updateLetterBoards = (letterToUpdate) => {
+              currentBoard[currentRow][iOfGuess] = letterToUpdate;
+              for (let j = 0; j < 3; j++) {
+                const indexToUpdate = currentKeyboard[j].findIndex(
+                  (ch) => ch.letter === guessedChar
+                );
+                currentKeyboard[j][indexToUpdate] = letterToUpdate;
+              }
             };
-            updateLetterBoards(letterToUpdate);
-          }
-          if (iOfWordToGuess >= 0) {
-            if (wordToGuess[iOfGuess] === c.letter) {
+
+            if (iOfWordToGuess < 0) {
               const letterToUpdate = {
-                state: "rightPos",
-                letter: c.letter,
-              };
-              updateLetterBoards(letterToUpdate);
-            } else {
-              const letterToUpdate = {
-                state: "wrongPos",
+                state: "wrongLetter",
                 letter: c.letter,
               };
               updateLetterBoards(letterToUpdate);
             }
-          }
-        });
-        setCurrentLetter(0);
-
-        if (guessedWord === wordToGuess)
-          toast({
-            title: "Great Job!",
-            position: "top",
-            description: `You got the word in ${currentRow + 1} turns.`,
-            duration: 2000,
+            if (iOfWordToGuess >= 0) {
+              if (wordToGuess[iOfGuess] === c.letter) {
+                const letterToUpdate = {
+                  state: "rightPos",
+                  letter: c.letter,
+                };
+                updateLetterBoards(letterToUpdate);
+              } else {
+                const letterToUpdate = {
+                  state: "wrongPos",
+                  letter: c.letter,
+                };
+                updateLetterBoards(letterToUpdate);
+              }
+            }
           });
-        if (currentRow < 5) setCurrentRow((prev) => prev + 1);
+          setCurrentLetter(0);
+
+          if (guessedWord === wordToGuess) {
+            toast({
+              title: "Great Job!",
+              position: "top",
+              description: `You got the word in ${currentRow + 1} turns!
+               Press RESTART for a new game.`,
+              duration: 4000,
+              status: "success",
+            });
+            setIsOver(true);
+            setIsWin(true);
+          }
+          if (currentRow === 5 && guessedWord !== wordToGuess) {
+            setIsOver(true);
+            toast({
+              title: "Game over!",
+              position: "top",
+              description:
+                "You ran out of guesses! Press RESTART for a new game!",
+              duration: 4000,
+              status: "error",
+            });
+          }
+          if (currentRow < 5) setCurrentRow((prev) => prev + 1);
+        } else {
+          toast({
+            title: "Not in word list!",
+            position: "top",
+            description: `Your word is not in the word list! Choose again.`,
+            duration: 2000,
+            status: "warning",
+          });
+        }
       } else {
         toast({
-          title: "Not in word list!",
+          title: "Word is too short!",
           position: "top",
-          description: `Your word is not in the word list! Choose again.`,
+          description: `Input more letters!`,
           duration: 2000,
           status: "warning",
         });
       }
-    } else {
-      toast({
-        title: "Word too short!",
-        position: "top",
-        description: `Your word is too short`,
-        duration: 2000,
-        status: "warning",
-      });
     }
   }
-
   function pressBackspace() {
     let currentBoard = [...board];
-    if (currentLetter >= 1) {
+    if (currentLetter >= 1 && !isOver) {
       currentBoard[currentRow][currentLetter - 1] = {
         state: "init",
         letter: "",
@@ -187,7 +208,7 @@ function App() {
     </VStack>
   );
   const gameKeyBoard = (
-    <VStack>
+    <VStack pb="20px">
       {keyboard.map((row) => {
         return (
           <HStack key={nanoid()}>
@@ -222,12 +243,19 @@ function App() {
     </VStack>
   );
 
+  const { width, height } = useWindowSize();
+
   return (
-    <Center h="100vh">
+    <Center h="100vh" w="100vw" bg="gray.800">
       <VStack h="100%" gap="20px">
         <Spacer />
-        <Heading>Yet Another Wordle 📚</Heading>
-        <Text color="gray.500">Word to guess: {wordToGuess}</Text>
+        <Text fontWeight="bold" fontSize="2rem" color="white" pb="20px">
+          Yet Another Wordle 📚
+        </Text>
+        {/* <Text>{wordToGuess}</Text> */}
+        {/* <Text>Current row: {currentRow}</Text> */}
+        {/* <Text>Is Over: {isOver ? "true" : "false"}</Text> */}
+        {/* <Text>Is Win: {isWin ? "true" : "false"}</Text> */}
         {gameBoard}
         {gameKeyBoard}
         <Button
@@ -241,14 +269,26 @@ function App() {
             setKeyboard(setEmptyKeyboard());
             setCurrentRow(0);
             setCurrentLetter(0);
+            setIsOver(false);
+            setIsWin(false);
           }}
           colorScheme="red"
-          w="100%"
+          w="80%"
         >
           RESTART
         </Button>
         <Spacer />
       </VStack>
+      {isOver && isWin && (
+        <Confetti
+          recycle={false}
+          initialVelocityY={30}
+          initialVelocityX={10}
+          numberOfPieces={600}
+          tweenDuration={1}
+          confettiSource={{ x: width / 2 - 50, y: height, w: 100, h: 10 }}
+        />
+      )}
     </Center>
   );
 }
